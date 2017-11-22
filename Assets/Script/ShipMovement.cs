8fb8633bc3dbe510d712a6e5622818555f26ac85
jdavidrevillad <jdavidrevillad@gmail.com>
2017-11-22 17:21:57 -0400

    first upload again

---
+++ Assets/Script/ShipMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour
+{
+
+    public float MovementSpeed = 1.0f;
+    public int invert = -1; //1 for standar -1 for invert
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position += (new Vector3(Input.GetAxis("Horizontal"), invert *Input.GetAxis("Vertical"), 0))*MovementSpeed*Time.deltaTime;
+	}
+}
---
+++ Assets/Script/ShootForward.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootForward : MonoBehaviour {
+
+    public Rigidbody Bullet;
+    public float Velocity = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody NewBullet = Instantiate(Bullet, transform.position, transform.rotation) as Rigidbody;
+            NewBullet.AddForce(transform.forward * Velocity, ForceMode.VelocityChange);
+        }
+	}
+}
